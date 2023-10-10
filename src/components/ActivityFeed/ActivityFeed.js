8ae6fc92ef54c6172e7f4947df2bd97cbaa3a4ce
KMaleesha <maleeshakumarasinghe@gmail.com>
2023-10-10 11:39:37 +0530

    task box and activity feed

--- src/components/ActivityFeed/ActivityFeed.js
+++ src/components/ActivityFeed/ActivityFeed.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './ActivityFeed.css';
 
+const activities = [
+    { id: 1, author: 'Kushantha Charuka', action: 'created', content: 'Contract #00124 need John Beige’s signature', timestamp: 'Sep 16, 2022 at 11:30 AM' },
+    { id: 2, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium neque', timestamp: 'Sep 16, 2022 at 11:45 AM' },
+    // ... Add other activities here
+];
+
+const ActivityCard = ({ activity }) => (
+    <div className="activity-card">
+        {activity.author && <strong>{activity.author} {activity.action}</strong>}
+        <p>{activity.content}</p>
+        <span className="timestamp">{activity.timestamp}</span>
+    </div>
+);
+
 const ActivityFeed = () => {
     return (
         <div className="activityFeed">
             <div className="contentWrapper">
-            <h2>Activity Feed</h2>
-            {/* ... Add activity items ... */}
+                <h2>Activity Feed</h2>
+                {activities.map((activity) => (
+                    <ActivityCard key={activity.id} activity={activity} />
+                ))}
             </div>
         </div>
     );
-}
+};
 
 export default ActivityFeed;
--- src/components/TaskBox/TaskBox.js
+++ src/components/TaskBox/TaskBox.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TaskBox.css';
 
+const tasksData = [
+    { id: 1, title: 'Contract #00124 need John Beige’s signature', status: 'Done', date: 'Sep 16' },
+    { id: 2, title: 'Franklin More’s professional review is pending', status: 'Done', date: 'Sep 12' },
+    // ... Add other tasks here
+];
+
+const TaskCard = ({ task }) => (
+    <div className="task-card">
+        <p>{task.title}</p>
+        <button className={`status-btn ${task.status.toLowerCase()}`}>{task.status}</button>
+        <span>{task.date}</span>
+    </div>
+);
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+    return (
+        <div className="pagination">
+            {pages.map((page) => (
+                <button
+                    key={page}
+                    className={currentPage === page ? 'active' : ''}
+                    onClick={() => onPageChange(page)}
+                >
+                    {page}
+                </button>
+            ))}
+        </div>
+    );
+};
+
 const TaskBox = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const tasksPerPage = 5;
+
+    const totalPages = Math.ceil(tasksData.length / tasksPerPage);
+    const tasksToShow = tasksData.slice(
+        (currentPage - 1) * tasksPerPage,
+        currentPage * tasksPerPage
+    );
+
     return (
         <div className="taskBox">
             <div className="contentWrapper">
-            <h2>Tasks</h2>
+                <h2>Tasks</h2>
+                {tasksToShow.map((task) => (
+                    <TaskCard key={task.id} task={task} />
+                ))}
+                <Pagination
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onPageChange={setCurrentPage}
+                />
             </div>
-            
-            {/* ... Add list items, buttons, and pagination ... */}
         </div>
     );
-}
+};
 
 export default TaskBox;
