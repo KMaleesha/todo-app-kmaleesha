c7776652a28bb3d3e1453b6697796b8c10e72be0
KMaleesha <maleeshakumarasinghe@gmail.com>
2023-10-11 01:05:33 +0530

    task box done

--- src/components/NotificationBox/NotificationBox.js
+++ src/components/NotificationBox/NotificationBox.js
@@ -5,7 +5,7 @@ const NotificationBox = () => {
     return (
         <div className="notificationBox">
             <div className="contentWrapper">
-                <h1 className="welcomeText">Welcome back, John Doe</h1>
+                <h3 className="welcomeText">Welcome back, John Doe</h3>
                 <p>The end of the year is coming. Are you planning your performance interviews? You can do this super efficiently with Acmy. </p>
                 <p><a href="#">Look here for more information</a></p>
                 <button className="closeButton">X</button>
--- src/components/TaskBox/TaskBox.js
+++ src/components/TaskBox/TaskBox.js
@@ -1,60 +1,116 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './TaskBox.css';
 
-const tasksData = [
-    { id: 1, title: 'Contract #00124 need John Beige’s signature', status: 'Done', date: 'Sep 16' },
-    { id: 2, title: 'Franklin More’s professional review is pending', status: 'Done', date: 'Sep 12' },
-    // ... Add other tasks here
-];
+const TaskCard = ({ task }) => {
+    const [completed, setCompleted] = useState(task.completed);
 
-const TaskCard = ({ task }) => (
-    <div className="task-card">
-        <p>{task.title}</p>
-        <button className={`status-btn ${task.status.toLowerCase()}`}>{task.status}</button>
-        <span>{task.date}</span>
-    </div>
-);
+    const formatDate = (isoDate) => {
+        const options = { month: 'short', day: 'numeric' };
+        return new Date(isoDate).toLocaleDateString(undefined, options);
+    };
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+    const toggleStatus = () => {
+        setCompleted(!completed);
+    };
+    
+    const formattedDate = formatDate(task.createdAt);
 
+    const getBellIcon = (priority) => {
+        switch (priority) {
+            case 'High':
+                return <span className="bell-icon" style={{color: 'red'}}>🔔</span>;
+            case 'Medium':
+                return <span className="bell-icon" style={{color: 'yellow'}}>🔔</span>;
+            case 'Low':
+                return <span className="bell-icon" style={{color: 'blue'}}>🔔</span>;
+            default:
+                return null;
+        }
+    }
     return (
-        <div className="pagination">
-            {pages.map((page) => (
+        <div className="task-card">
+            <div className="task-info">
+                <div className="task-priority">
+                    {getBellIcon(task.priority)}
+                </div>
+                <p>{task && task.todo}</p>
+            </div>
+            <div className="status-button">
                 <button
-                    key={page}
-                    className={currentPage === page ? 'active' : ''}
-                    onClick={() => onPageChange(page)}
+                    className={`status-btn ${completed ? 'done' : 'in-progress'}`}
+                    onClick={toggleStatus}
                 >
-                    {page}
+                    {completed ? 'Done' : 'In Progress'}
                 </button>
-            ))}
+                <p className="date">{formattedDate}</p>
+            </div>
         </div>
-    );
+    );       
 };
 
 const TaskBox = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const tasksPerPage = 5;
+    const [currentPage, setCurrentPage] = useState(0);
+    const tasksPerPage = 8;
+    const [tasksData, setTasksData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get('https://6363c8f68a3337d9a2e7d805.mockapi.io/api/to-do')
+            .then((response) => {
+                setTasksData(response.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            });
+    }, []);
 
     const totalPages = Math.ceil(tasksData.length / tasksPerPage);
-    const tasksToShow = tasksData.slice(
-        (currentPage - 1) * tasksPerPage,
-        currentPage * tasksPerPage
-    );
+
+    const handleNextPage = () => {
+        setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
+    };
+
+    const handlePrevPage = () => {
+        setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+    };
+
+    const handleNumberedPage = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const startIndex = currentPage * tasksPerPage;
+    const endIndex = startIndex + tasksPerPage;
+    const tasksToShow = tasksData.slice(startIndex, endIndex);
 
     return (
         <div className="taskBox">
             <div className="contentWrapper">
                 <h2>Tasks</h2>
-                {tasksToShow.map((task) => (
-                    <TaskCard key={task.id} task={task} />
-                ))}
-                <Pagination
-                    currentPage={currentPage}
-                    totalPages={totalPages}
-                    onPageChange={setCurrentPage}
-                />
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <>
+                        {tasksToShow.map((task) => (
+                            <TaskCard key={task.id} task={task} />
+                        ))}
+                        <div className="pagination">
+                            <button onClick={handlePrevPage}>{'<'}</button>
+                            {[...Array(totalPages)].map((_, index) => (
+                                <button
+                                    className={`pagination-button ${currentPage === index ? 'active' : ''}`}
+                                    key={index}
+                                    onClick={() => handleNumberedPage(index)}
+                                >
+                                    {index + 1}
+                                </button>
+                            ))}
+                            <button onClick={handleNextPage}>{'>'}</button>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
